d88d7efab6a936dfd67b094cb16f29c12b6b73f5
João Victor <joaosantos.senai564@gmail.com>
2024-05-08 15:56:51 -0300

    procurar por nome

--- index.js
+++ index.js
@@ -14,34 +14,38 @@ const pool = new Pool({
 
 app.use(express.json());
 
-async function calcularVencedor(heroi01_id, heroi02_id) {
-    const hero1 = await pool.query('SELECT * FROM herois WHERE id = $1', [heroi01_id]);
-    const hero2 = await pool.query('SELECT * FROM herois WHERE id = $1', [heroi02_id]);
 
-    const totalDanoHero1 = hero1.rows[0].dano * hero1.rows[0].nivel;
-    const totalDanoHero2 = hero2.rows[0].dano * hero2.rows[0].nivel;
+//em caso de empate o nmr1 vence!
+ async function calcularVencedor(heroi01_id, heroi02_id) {
+     const hero1 = await pool.query('SELECT * FROM herois WHERE id = $1', [heroi01_id]);
+     const hero2 = await pool.query('SELECT * FROM herois WHERE id = $1', [heroi02_id]);
 
-    const hpHero1 = hero1.rows[0].hp;
-    const hpHero2 = hero2.rows[0].hp;
+     const totalDanoHero1 = hero1.rows[0].dano * hero1.rows[0].nivel;
+     const totalDanoHero2 = hero2.rows[0].dano * hero2.rows[0].nivel;
 
-    if (totalDanoHero1 >= hpHero2) {
-        return heroi01_id;
-    } else if (totalDanoHero2 >= hpHero1) {
-        return heroi02_id;
-    } else {
-        const danoTotalHero1 = totalDanoHero1 + hpHero1;
-        const danoTotalHero2 = totalDanoHero2 + hpHero2;
+     const hpHero1 = hero1.rows[0].hp;
+     const hpHero2 = hero2.rows[0].hp;
 
-        if (danoTotalHero1 > danoTotalHero2) {
-            return heroi01_id;
-        } else if (danoTotalHero2 > danoTotalHero1) {
-            return heroi02_id;
-        } else {
-            // Se houver um empate na soma de danos, retorna null
-            return null;
-        }
-    }
-}
+     if (totalDanoHero1 > hpHero2) {
+         return heroi01_id;
+     } else if (totalDanoHero2 > hpHero1) {
+         return heroi02_id;
+     } else {
+         const danoTotalHero1 = totalDanoHero1 + hpHero1;
+         const danoTotalHero2 = totalDanoHero2 + hpHero2;
+
+         if (danoTotalHero1 > danoTotalHero2) {
+             return heroi01_id;
+         } else if (danoTotalHero2 > danoTotalHero1) {
+             return heroi02_id;
+         } else {
+             
+             return null;
+         }
+     }
+ }
+
+
 
 
 
@@ -100,11 +104,11 @@ app.get('/herois', async (req, res) => {
   });
 
   app.post('/herois', async (req, res) => {
-    const { nome, poder, nivel, hp  } = req.body;
-    const query = 'INSERT INTO herois (  nome, poder, nivel, hp  ) VALUES ($1, $2, $3, $4) RETURNING *';
-    const values = [nome, poder, nivel, hp ];
+    const { nome, poder, dano, nivel, hp  } = req.body;
+    const query = 'INSERT INTO herois (  nome, poder, dano, nivel, hp  ) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+    const values = [nome, poder, dano, nivel, hp ];
   try {
-        const result = await pool.query('INSERT INTO herois (  nome, poder, nivel, hp  ) VALUES ($1, $2, $3, $4) RETURNING *', [ nome, poder, nivel, hp]);
+        const result = await pool.query('INSERT INTO herois (  nome, poder, dano, nivel, hp  ) VALUES ($1, $2, $3, $4, $5) RETURNING *', [ nome, poder, dano, nivel, hp]);
         res.status(201).json({
             status: 'success',
             message: 'Herois criado com sucesso🕷',
@@ -134,11 +138,11 @@ app.get('/herois', async (req, res) => {
 
  app.put('/herois/:id', async (req, res) => {
     const id = req.params.id;
-    const { nome, poder, nivel, hp  } = req.body;
-    const query = 'UPDATE herois SET nome=$1, poder=$2, nivel=$3, hp=$4  WHERE id=$5';
-    const values = [nome, poder, nivel, hp ];
+    const { nome, poder, dano, nivel, hp  } = req.body;
+    const query = 'UPDATE herois SET nome=$1, poder=$2, dano=$3 nivel=$4, hp=$5  WHERE id=$6';
+    const values = [nome, poder, dano, nivel, hp ];
   try {
-        const result = await pool.query('UPDATE herois SET nome=$1, poder=$2, nivel=$3, hp=$4  WHERE id=$5', [ nome, poder, nivel, hp, id]);
+        const result = await pool.query('UPDATE herois SET nome=$1, poder=$2, dano=$3 nivel=$4, hp=$5  WHERE id=$6', [ nome, poder, dano, nivel, hp, id]);
         res.status(201).json({
             status: 'success',
             message: 'Herois alterado com sucesso🕷',
@@ -150,91 +154,7 @@ app.get('/herois', async (req, res) => {
     }
   });
 
-//   app.get('/batalhas/:heroi01_id/:heroi02_id', async (req, res) => {
-//     const { heroi01_id, heroi02_id } = req.params;
 
-//     try {
-//         const vencedorId = await calcularVencedor(heroi01_id, heroi02_id);
-
-//         if (vencedorId) {
-//             res.json({
-//                 status: 'success',
-//                 message: `O herói vencedor é o: ${vencedorId}`,
-//                 vencedor_id: vencedorId,
-//             });
-//         } else {
-//             res.json({
-//                 status: 'success',
-//                 message: 'A batalha terminou em empate',
-//             });
-//         }
-//     } catch (error) {
-//         console.error('Erro ao calcular vencedor da batalha', error);
-//         res.status(500).send('Erro ao calcular vencedor da batalha');
-//     }
-// });
-
-// app.get('/batalhas/:heroi01_id/:heroi02_id', async (req, res) => {
-//     const { heroi01_id, heroi02_id } = req.params;
-
-//     try {
-//         const query = 'SELECT vencedor_id, herois1.nome AS nome_heroi1, herois2.nome AS nome_heroi2 FROM batalhas INNER JOIN herois AS herois1 ON batalhas.heroi01_id = herois1.id INNER JOIN herois AS herois2 ON batalhas.heroi02_id = herois2.id WHERE batalhas.heroi01_id = $1 AND batalhas.heroi02_id = $2;', [id];
-//         const result = await pool.query(query, [heroi01_id, heroi02_id]);
-
-//         if (result.rowCount > 0) {
-//             const vencedor = result.rows[0];
-//             res.json({
-//                 status: 'success',
-//                 message: 'O herói vencedor é o: ${vencedorId}',
-//                 vencedor_id: vencedor.id,
-//                 vencedor_nome: vencedor.nome,
-//             });
-//         } else {
-//             res.json({
-//                 status: 'success',
-//                 message: 'A batalha terminou em empate',
-//             });
-//         }
-//     } catch (error) {
-//         console.error('Erro ao calcular vencedor da batalha', error);
-//         res.status(500).send('Erro ao calcular vencedor da batalha');
-//     }
-// });
-
-// app.get('/batalhas/:heroi01_id/:heroi02_id', async (req, res) => {
-//     const { heroi01_id, heroi02_id } = req.params;
-
-//     try {
-//         const vencedorId = await calcularVencedor(heroi01_id, heroi02_id);
-
-//         if (vencedorId) {
-//             const query = 'SELECT batalhas.id, hero1_id, hero2_id, winner_id, heroes.name as winner_name, heroes.power as winner_power, heroes.level as winner_level, heroes.hp as winner_hp FROM battles INNER JOIN heroes ON battles.winner_id = heroes.id'';
-//             const result = await pool.query(query, [vencedorId]);
-            
-//             if (result.rowCount > 0) {
-//                 const nomeVencedor = result.rows[0].nome_vencedor;
-//                 res.json({
-//                     status: 'success',
-//                     message: `O herói vencedor é: ${nomeVencedor}`,
-//                     vencedor_nome: nomeVencedor,
-//                 });
-//             } else {
-//                 res.json({
-//                     status: 'success',
-//                     message: 'Herói vencedor não encontrado',
-//                 });
-//             }
-//         } else {
-//             res.json({
-//                 status: 'success',
-//                 message: 'A batalha terminou em empate',
-//             });
-//         }
-//     } catch (error) {
-//         console.error('Erro ao calcular vencedor da batalha', error);
-//         res.status(500).send('Erro ao calcular vencedor da batalha');
-//     }
-// });
 
 app.get('/batalhas/:heroi01_id/:heroi02_id', async (req, res) => {
     const { heroi01_id, heroi02_id } = req.params;
@@ -244,22 +164,53 @@ app.get('/batalhas/:heroi01_id/:heroi02_id', async (req, res) => {
 
         await pool.query('INSERT INTO batalhas (heroi01_id, heroi02_id, vencedor_id) VALUES ($1, $2, $3)', [heroi01_id, heroi02_id, vencedorId]);
 
-        //exibe o vencedor (todos os dados) e a mensagem de sucesso de registro
-
-        const { rows } = await pool.query('SELECT * FROM herois WHERE id = $1', [vencedorId]);
         res.json({
     status: 'success',
     message: `O herói vencedor é o: ${vencedorId}`,
     vencedor_id: vencedorId,
         });
     } catch (error) {
-        console.error('Erro ao deletar heroi', error);
+        console.error('Erro ao execultar batalhas', error);
         res.status(500).send('Erro ao deletar Heroi');
     }
   });
 
-  //
-  
+
+  app.get('/batalhas', async (req, res) => {
+    try {
+        const { rows } = await pool.query('SELECT * FROM batalhas');
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/batalhas/herois', async (req, res) => {
+    try {
+        const { rows } = await pool.query('SELECT batalhas.id, heroi01_id, heroi02_id, vencedor_id, herois.nome as nomeVencedor, herois.poder as poderVencedor, herois.dano as danoVencedor, herois.nivel as nivelVencedor, herois.hp as hpVencedor FROM batalhas INNER JOIN herois ON batalhas.vencedor_id = herois.id');
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/batalhas/herois/nome/:nome', async (req, res) => {
+    const { nome } = req.params
+
+    try {
+        const result = await pool.query('SELECT b.id as batalhas, heroi01.nome AS nome_heroi_1, heroi01.poder AS poder_heroi_1, heroi02.nome AS nome_heroi_2, heroi02.poder AS poder_heroi_2, vencedor.nome AS nome_vencedor FROM batalhas b JOIN herois heroi01 ON b.heroi01_id = heroi01.id JOIN herois heroi02 ON b.heroi02_id = heroi02.id LEFT JOIN herois vencedor ON b.vencedor_id = vencedor.id WHERE heroi01.nome = $1 OR heroi01.nome = $1',
+    [nome]);
+        res.status(201).json({
+            status: 'success',
+            message: 'Herois alterado com sucesso🕷',
+            dados: result.rows[0],
+        });
+    } catch (error) {
+        console.error('Erro ao inserir Heroi', error);
+        res.status(500).send('Erro ao inserir Heroi');
+    }
+  });
+
 
 // Iniciando o servidor http://localhost:5000
 app.listen(port, () => {
